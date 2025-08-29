b9421a90f2e2fcdfde8679dd4122383d31dd9e3e
TsengFan <22421357@zju.edu.cn>
2025-08-29 17:04:29 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
 
-// 你的仓库名称
-const REPO_NAME = ".";
+const REPO_NAME = "Bartender";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // 👇 新增这个 base 配置
-  base: `/${REPO_NAME}/`, 
-  plugins: [react(), tailwindcss()],
+  base: `/Bartender/`, 
+  plugins: [react()],
 });
 // export default defineConfig(({ mode }) => {
 //   // 加载环境变量
