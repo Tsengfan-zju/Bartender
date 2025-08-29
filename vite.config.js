90326a5a9d8a733f7ab529b1e54e8ff28ff36566
Tsengfan <22421357@zju.edu.cn>
2025-08-29 16:49:00 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // 你的仓库名称
