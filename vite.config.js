452f6258859e2359046fd63e51d9c592b449443d
Tsengfan <22421357@zju.edu.cn>
2025-08-29 16:51:34 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 // 你的仓库名称
-const REPO_NAME = "Bartender";
+const REPO_NAME = ".";
 
 // https://vite.dev/config/
 export default defineConfig({
